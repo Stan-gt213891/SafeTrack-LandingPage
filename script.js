0d26170233cca45d0fb4e2255d9ab825f868394e
STANLEY JEREMY GUTIERREZ TUME <jeremygurierreztume@gmail.com>
2026-05-29 21:56:13 -0500

    Avance integracion frontend backend

--- script.js
+++ script.js
@@ -77,8 +77,11 @@ if (loginButton) {
       const data = await response.json();
 
       if (response.ok) {
-        localStorage.setItem("safeTrackToken", data.token);
-        message.textContent = "Inicio de sesión exitoso.";
+ localStorage.setItem("safeTrackToken", data.token);
+localStorage.setItem("safeTrackEmail", data.email);
+localStorage.setItem("safeTrackFullName", data.fullName);
+localStorage.setItem("safeTrackRole", data.role);
+  message.textContent = "Inicio de sesión exitoso.";
         setTimeout(() => {
           window.location.href = "dashboard.html";
         }, 1000);
@@ -89,4 +92,66 @@ if (loginButton) {
       message.textContent = "Error al conectar con el backend.";
     }
   });
+}
+const dashboardEmail = document.getElementById("dashboardEmail");
+
+if (dashboardEmail) {
+  const savedEmail = localStorage.getItem("safeTrackEmail");
+
+  if (!savedEmail) {
+    window.location.href = "login.html";
+  } else {
+    dashboardEmail.textContent = savedEmail;
+  }
+}
+
+const logoutButton = document.getElementById("logoutButton");
+
+if (logoutButton) {
+  logoutButton.addEventListener("click", () => {
+    localStorage.removeItem("safeTrackToken");
+    localStorage.removeItem("safeTrackEmail");
+    window.location.href = "login.html";
+  });
+}
+const panicButton = document.getElementById("panicButton");
+
+if (panicButton) {
+  panicButton.addEventListener("click", async () => {
+    try {
+      const response = await fetch(`${API_URL}/alerts/panic`, {
+        method: "POST"
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert(data.message || "Alerta activada correctamente.");
+      } else {
+        alert("No se pudo activar la alerta.");
+      }
+    } catch (error) {
+      alert("Error al conectar con el backend.");
+    }
+  });
+}
+const dashboardLocation = document.getElementById("dashboardLocation");
+
+if (dashboardLocation) {
+  async function loadDashboardLocation() {
+    try {
+      const response = await fetch(`${API_URL}/tracking/1`);
+      const data = await response.json();
+
+      if (response.ok) {
+        dashboardLocation.textContent = `Lat: ${data.latitude}, Lng: ${data.longitude}`;
+      } else {
+        dashboardLocation.textContent = "No se pudo cargar ubicación";
+      }
+    } catch (error) {
+      dashboardLocation.textContent = "Error al conectar ubicación";
+    }
+  }
+
+  loadDashboardLocation();
 }
